0e805a7010d198301b8d81349780f7c14d316e2c
Herschel Herring <hbradherring@aol.com>
2019-04-06 16:32:52 -0400

    Fixed hashrouter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <HashRouter>
